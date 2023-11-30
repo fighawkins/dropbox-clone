a2f2113518ae1643e26a31d4c7730d86772256c9
Fig Hawkins <fighawkins@Figs-MacBook-Air.local>
2023-11-30 14:16:28 -0800

    homepage created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 import { UserButton } from "@clerk/nextjs";
-
+import Link from "next/link";
 import Image from 'next/image'
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <main >
-      <h1 className="font-bold">Dropbox Clone</h1>
+      <div>
+        <div className="p-10 flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox <br />
+            <br />
+            Storing everything for you and your business needs
+          </h1>
+          <p className="pb-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem non nesciunt, tempora nam nihil magnam minima ea accusantium architecto 
+            eos vitae labore odio cupiditate iure perferendis id adipisci accusamus quas.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem non nesciunt, tempora nam nihil magnam minima ea accusantium architecto eos vitae labore odio cupiditate iure perferendis id adipisci accusamus quas.
+          </p>
+          <Link href="/dashboard"
+            className="flex cursor-pointer bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+        <div>
+          <video autoPlay loop muted className="rounded-lg" >
+            <source
+            src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+            type="video/mp4"
+            />
+          </video>
+          
+          
+        </div>
+      </div>
     </main>
   )
 }
